fa4cc80a3b0efb70db107d8253e5e6ceb1e345ef
admintotoro <128627896+admintotoro@users.noreply.github.com>
2023-03-27 03:33:13 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'qrcode-app-cache-v1';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icons/icon-72x72.png',
+        '/icons/icon-96x96.png',
+        '/icons/icon-128x128.png',
+        '/icons/icon-144x144.png',
+        '/icons/icon-152x152.png',
+        '/icons/icon-192x192.png',
+        '/icons/icon-384x384.png',
+        '/icons/icon-512x512.png',
+        'https://cdn.bootcdn.net/ajax/libs/jquery/3.6.0/jquery.min.js',
+        'https://cdn.bootcdn.net/ajax/libs/qrcodejs/1.0.0/qrcode.min.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(keyList.map(key => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
